55ae6c44d80f8023b81ed8e699a84b9d7bbed58f
lucianogarcia93 <lucianoxeneize93@gmail.com>
2026-04-08 09:52:57 -0300

    seguimos ajustando login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,20 +9,21 @@
 <body class="bg-blue-950 flex items-center justify-center min-h-screen px-4">
 
     <!-- CONTENEDOR GENERAL -->
-    <div class="w-full sm:max-w-md mx-auto px-4 sm:px-6">
+    <div class="w-full sm:max-w-lg mx-auto px-4 sm:px-6">
 
         <!-- TITULO -->
         <div class="text-center mb-6 sm:mb-8">
             <h1 class="text-6xl sm:text-6xl font-extrabold text-white leading-tight tracking-tight sm:tracking-[0.35em]">
                 SIGAE
             </h1>
-            <p class="text-white text-sm sm:text-base mt-2 sm:mt-3 leading-snug text-center tracking-tight sm:tracking-[0.11em] uppercase">
+            <!-- Siempre debajo de SIGAE -->
+            <p class="text-white text-sm sm:text-base mt-2 sm:mt-3 leading-snug sm:leading-normal text-center tracking-tight sm:tracking-[0.11em] uppercase">
                 Sistema Integral de Gestión Académica Escolar
             </p>
         </div>
 
         <!-- CARD LOGIN -->
-        <div class="bg-white shadow-2xl rounded-2xl p-6 sm:p-8 min-h-[70vh] sm:min-h-auto flex flex-col justify-center">
+        <div class="bg-white shadow-2xl rounded-2xl p-5 sm:p-8 min-h-[60vh] sm:min-h-[50vh] flex flex-col justify-center">
 
             <!-- Errores -->
             @if ($errors->any())
@@ -47,7 +48,7 @@
                         type="email" 
                         name="email" 
                         required
-                        class="w-full px-3 py-3 sm:px-4 sm:py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-900 focus:outline-none transition"
+                        class="w-full sm:w-full px-3 py-3 sm:px-4 sm:py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-900 focus:outline-none transition"
                     >
                 </div>
 
@@ -57,13 +58,13 @@
                         type="password" 
                         name="password" 
                         required
-                        class="w-full px-3 py-3 sm:px-4 sm:py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-900 focus:outline-none transition"
+                        class="w-full sm:w-full px-3 py-3 sm:px-4 sm:py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-900 focus:outline-none transition"
                     >
                 </div>
 
                 <button 
                     type="submit"
-                    class="w-full bg-blue-900 hover:bg-blue-800 text-white py-3 rounded-xl transition duration-200 font-semibold shadow-md text-base sm:text-base"
+                    class="w-full bg-blue-900 hover:bg-blue-800 text-white py-3 sm:py-3 rounded-xl transition duration-200 font-semibold shadow-md text-base sm:text-base"
                 >
                     Ingresar
                 </button>
