d52af738ec475c234b433ebd7689f7f1881ae6fd
lucianogarcia93 <lucianoxeneize93@gmail.com>
2026-04-10 10:26:08 -0300

    agrego turno en columna curso de vista solicitudes

--- resources/views/academica/solicitudes/index.blade.php
+++ resources/views/academica/solicitudes/index.blade.php
@@ -67,7 +67,7 @@
                     <td class="px-6 py-4">{{ $s->apellido }}</td>
                     <!--<td class="px-6 py-4">{{ $s->dni }}</td>-->
                     <td class="px-6 py-4">{{ $s->email }}</td>
-                    <td class="px-6 py-4">{{ optional($s->curso)->division ?? '-' }} - {{ optional($s->curso->nivel)->nombre ?? '-' }}</td>
+                    <td class="px-6 py-4">{{ optional($s->curso)->division ?? '-' }} - {{ optional($s->curso->nivel)->nombre ?? '-' }} - {{ optional($s->curso->turno)->nombre ?? '-' }}</td>
 
                     <!-- ESTADO -->
                     <td class="px-6 py-4">
